a91d9511dcc692649233c9314dfce88d3060a9be
Habiba chaab <habibachaab72@gmail.com>
2025-07-03 07:32:44 -0800

    Modification

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-amber-300">
+      <h1 className="text-3xl font-bold underline text-red-300">
         Hello Vite + React!
       </h1>
     </>
