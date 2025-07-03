bf313be28e4b109589ec7ad003899ab338489112
Zakaria El Guazzar <elguazzarzakaria10@gmail.com>
2025-07-03 16:47:05 +0100

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline text-amber-300">
-        Hello Vite + React!
+        Hello Vite + React Salama
       </h1>
     </>
   )
